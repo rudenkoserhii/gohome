2870e45f3b767446970777a0f8cfeaff6e772aeb
rudenkoserhii <rudenko.sergey@ukr.net>
2023-10-09 13:36:17 +0300

    first create

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Button>Click me</Button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/Button.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Button
   }
 }
 </script>
---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <button v-bind:type="type">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'MainButton',
+    props: {
+        type: {
+            type: String,
+            default: 'button'
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped></style>
